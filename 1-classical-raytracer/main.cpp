6bcac733942a383ed1395645fc3450008d89a761
goto-c <gotochihiro47@gmail.com>
2022-09-11 15:57:22 +0900

    show white image

--- 1-classical-raytracer/main.cpp
+++ 1-classical-raytracer/main.cpp
@@ -1,1 +1,16 @@
-int main() { return 0; }
+#include "image.h"
+#include "io.h"
+
+int main()
+{
+  const int WIDTH = 512;
+  const int HEIGHT = 512;
+  Image image(WIDTH, HEIGHT);
+  for (int i = 0; i < WIDTH; i++) {
+    for (int j = 0; j < HEIGHT; j++) {
+      image.addPixel(i, j, glm::vec3(1.0, 1.0, 1.0));
+    }
+  }
+  write_png("output.png", WIDTH, HEIGHT, image.getConstPtr());
+  return 0;
+}
